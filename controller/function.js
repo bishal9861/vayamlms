f6ae422241378222bead0754eb5dfa5aea2e984f
bishal9861 <bishalpatel9861@gmail.com>
2021-08-23 20:41:20 +0530

    update model

--- controller/function.js
+++ controller/function.js
@@ -1,33 +1,14 @@
 const db=require("../database/connect.js")
 const sequelize=require("../database/connect")
 const Student=require("../model/student")
+const fast2sms=require("fast-two-sms");
 exports.userLogin=(req,res,next)=>{
-    
     res.render('userlogin.ejs')
-      
 }
 exports.userSignup=(req,res,next)=>{
     res.render('usersignup.ejs')
 }
 exports.userLogged=(req,res,next)=>{
-    const {loginid,loginpassword}=req.body;
-    Student.findAll({
-        where:{
-            stud_email:loginid,
-            stud_password:loginpassword
-        }
-    }).then(
-        data=>{
-            if(data.length==0){
-                res.send("signup")
-            }else{
-                res.send("main page")
-            }
-        }
-    ).catch(
-        error=>{
-            console.log(error)
-        }
-    )
+    
     
 }
--- model/student.js
+++ model/student.js
@@ -3,46 +3,50 @@ const sequelize=require("../database/connect")
 const Student=sequelize.define("student",{
     stud_id:{
         type:Sequelize.INTEGER,
-        autoIncrement:true,
+        
         primaryKey:true,
         allowNull:false
 
     },
+    stud_name:{
+        type:Sequelize.STRING,
+        allowNull:true
+    },
     stud_username:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
     }
     ,
     stud_email:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
 
     },
     stud_parent:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
     },
     stud_gender:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
 
     },
     stud_location:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
     },
     stud_phone:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
 
     },
     stud_age:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
     },
     stud_password:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:true
     }
 
 
