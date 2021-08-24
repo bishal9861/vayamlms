970673ac6736710a7f9af8c50f40dfd10743ea55
Amlan2002 <iamamlan2002@gmail.com>
2021-08-24 16:05:08 +0530

    adding otp screen

--- public/js/userotp.js
+++ public/js/userotp.js
@@ -23,7 +23,6 @@ inputs.forEach(input => {
 
 function movetoNext(previousFieldID, current, nextFieldID, event) {
 	var x = event.keyCode;
-	if (x >= 96 && x <= 105 || x==8) {
 		if (current.value.length >= current.maxLength) {
 			if (x >= 96 && x <= 105) {
 				document.getElementById(nextFieldID).focus();
@@ -33,21 +32,8 @@ function movetoNext(previousFieldID, current, nextFieldID, event) {
 			if (current.value.length == 0) {
 				document.getElementById(previousFieldID).focus();
 			}
-		}
 	}
 }
 
 
 
-// document.getElementById('text1').onkeydown = function() {
-// 	if ( this.value.length == this.maxLength)
-// 	 document.getElementById('text2').focus();
-//   }
-// document.getElementById('text2').onkeydown = function() {
-// 	if (this.value.length == this.maxLength)
-// 	 document.getElementById('text3').focus();
-//   }
-// document.getElementById('text3').onkeydown = function() {
-// 	if (this.value.length == this.maxLength)
-// 	 document.getElementById('text4').focus();
-//   }
